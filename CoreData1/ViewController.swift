40434dc2c03083a23025dc116934bedd6d6e5f2f
ChiterP <63542970+ChiterP@users.noreply.github.com>
2021-05-15 21:51:36 +0300

    Initial Commit

---
+++ CoreData1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData1
+//
+//  Created by Алексей Сергейцев on 5/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
